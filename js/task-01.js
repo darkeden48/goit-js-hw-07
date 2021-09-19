b98aa0a988c8f386cf2e79da296a49800bf3a959
darkeden48 <dzalex72@list.ru>
2021-09-19 10:43:48 +0300

    1 times

---
+++ js/task-01.js
@@ -0,0 +1,7 @@
+const navEl=document.querySelector('ul');
+console.log(navEl);
+console.log(`В списке ${navEl.children.length} категории`);
+for (let i=0;i<3;i++){
+    console.log(navEl.children.length);
+    
+}
