b3081b3b855a2e576ff65acafa84aa4b108dbaf9
darkeden48 <dzalex72@list.ru>
2021-09-28 10:52:56 +0300

    4comp

--- js/task-04.js
+++ js/task-04.js
@@ -1,5 +1,17 @@
 const decr=document.querySelector('[data-action="decrement"]');
-const incr=document.querySelector('[data-action="increment"]')
+const incr=document.querySelector('[data-action="increment"]');
+const counterValue=document.getElementById("value");
 const increment=function(){
-    counterValue+=1
+    counterValue.textContent++;
+    console.log(counterValue);
+    
+};
+const decrement=function(){
+    counterValue.textContent--;
+    console.log(counterValue);
 }
+
+decr.addEventListener('click',decrement);
+incr.addEventListener('click',increment);
+
+
