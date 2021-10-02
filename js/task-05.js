56d647863101f263a696d5a9db69ea8baccc55f5
darkeden48 <dzalex72@list.ru>
2021-10-02 14:59:16 +0300

    Update task-05.js

---
+++ js/task-05.js
@@ -0,0 +1,7 @@
+const input=document.querySelector('input#name-input');
+const output=document.querySelector('span#name-output');
+console.log(output);
+console.log(input);
+const nameInput =function(){
+    
+}
